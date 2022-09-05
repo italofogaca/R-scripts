004e4e56802f5e335a5a36a1a5b607dc539a17c8
Italo Fogaça <108767749+italofogaca@users.noreply.github.com>
2022-09-05 12:27:51 -0300

    Add files via upload

---
+++ resolução atividade prática 2.R
@@ -0,0 +1,20 @@
+##Questão 1
+
+clientes <-c("Marcos de Aguiar", "Paula Miranda", "Emilio Santos", 
+              "Priscila de Albuquerque")
+
+##Questão 2
+#localizar o indice do nome a ser mudado e efetuar a mudança
+clientes[4] <-c("Pryscilla de Albuquerque")
+
+##Questão 3
+clientesval <-c(4000, 2500, 1200, 3400)
+
+salarios = interaction(clientes, '= R$' , clientesval)
+
+##Questão 4
+
+clientesvalatt = c(clientesval*0.30)
+salariosatt = interaction(clientes, '= R$', clientesvalatt)
+
+
